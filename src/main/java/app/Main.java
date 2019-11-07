0be947693287e7f57db5d2ba6e5b501b35b273be
Kamil Grabowski <grabowski.kamil@gmail.com>
2019-11-07 00:18:32 -0700

    Change window dimension to better suit the game

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,8 +8,8 @@ import game.MyGame;
 public class Main {
     public static void main(String[] args) {
         var config = new LwjglApplicationConfiguration();
-        config.width = 640;
-        config.height = 480;
+        config.width = 600;
+        config.height = 800;
         config.title = "Java Basics";
 
         new LwjglApplication(new MyGame(), config);
