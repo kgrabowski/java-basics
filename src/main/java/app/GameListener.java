dc20cfad44ad804c32abff696d23a56958718c06
Kamil Grabowski <grabowski.kamil@gmail.com>
2019-11-06 19:37:27 -0700

    Configure basic libGdx window

---
+++ src/main/java/app/GameListener.java
@@ -0,0 +1,13 @@
+package app;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GameListener extends ApplicationAdapter {
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+}
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Everything works!");
+        var config = new LwjglApplicationConfiguration();
+        config.forceExit = false;
+        config.width = 640;
+        config.height = 480;
+        config.title = "Java Basics";
+
+        new LwjglApplication(new GameListener(), config);
     }
 }
