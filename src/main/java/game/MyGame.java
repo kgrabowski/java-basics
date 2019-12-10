e1259d7b5d90f07b9a85a133420542937678c0ca
Kamil Grabowski <grabowski.kamil@gmail.com>
2019-12-10 01:02:28 -0700

    Move player with arrow keys

--- src/main/java/game/MyGame.java
+++ src/main/java/game/MyGame.java
@@ -3,6 +3,10 @@ package game;
 import app.GameListener;
 
 public class MyGame extends GameListener {
+    // We define fields directly inside the class, like this:
+    float x = 260;
+    float y = 200;
+
     @Override
     protected void init() {
         // The code to run once at the start of the game goes here
@@ -11,5 +15,10 @@ public class MyGame extends GameListener {
     @Override
     protected void update() {
         // The code to run repeatedly during the game goes here
+        var dx = getInputX() * 3;
+        var dy = getInputY() * 3;
+        x = x + dx;
+        y = y + dy;
+        drawPlayerPlane(x, y);
     }
 }
